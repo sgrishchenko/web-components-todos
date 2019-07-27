303a3ba03809844ee0e3144da9a642bd16a01754
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2019-07-27 22:51:44 +0300

    removed model from simple implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './app/app.js'
-import {TodoList} from "./model/todoList";
-import {context} from "./context";
-
-const todoList = new TodoList();
-context.todoList = todoList;
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
