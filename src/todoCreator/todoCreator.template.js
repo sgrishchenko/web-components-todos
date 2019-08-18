dddb343bb2f6058fc5cedb93eae27271967f629e
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2019-08-18 20:39:22 +0300

    try fix flex of todoCreator

--- src/todoCreator/todoCreator.template.js
+++ src/todoCreator/todoCreator.template.js
@@ -7,12 +7,12 @@ todoCreatorTemplate.innerHTML = `
     }
     
     #title-input {
-      flex-basis: 75%;
+      flex: 1 75%;
       padding: 10px;
       font-size: 16px;
     }
     #add-button {
-      flex-basis: 25%;
+      flex: 1 25%;
       color: #505050;
       font-size: 16px;
     }
